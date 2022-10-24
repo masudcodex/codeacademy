1261a89ace59c33a17387d542f6da4ada29faf12
masudcodex <forester.masud@gmail.com>
2022-10-25 03:04:46 +0600

    react app initiated, react router dom, react bootstrap and firebase has been setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+//Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8PhGdz1JQKnBmyun8kCfxWW784lj2zds",
+  authDomain: "codeacademy-2amrx.firebaseapp.com",
+  projectId: "codeacademy-2amrx",
+  storageBucket: "codeacademy-2amrx.appspot.com",
+  messagingSenderId: "325233291855",
+  appId: "1:325233291855:web:caf45f7aaddcc6b40c303a"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
