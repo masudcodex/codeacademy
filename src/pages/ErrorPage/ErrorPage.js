4ee28ebb6a27253ef7288bf0d77d675ff82bde5b
masudcodex <forester.masud@gmail.com>
2022-10-27 13:58:18 +0600

    404 page added and designed

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import image from '../../assets/images/404.gif';
 const ErrorPage = () => {
     return (
-        <div>
-            <h2>404 error! Page Not Found</h2>
+        <div className="cover-container w-100 h-100 p-3 mx-auto">
+            <main className="px-3 text-center">
+                <img src={image} alt="" />
+                <h1>We can’t find the page you’re looking for</h1>
+                <p className="lead">
+                <Link className='fw-bold' to="/">Back to Home</Link>
+                </p>
+            </main>
         </div>
     );
 };
