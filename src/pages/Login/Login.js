a8dc0049dd008373a00e3ce6b36378c56813a17a
masudcodex <forester.masud@gmail.com>
2022-10-28 12:07:56 +0600

    login redirect to private route issue fixed

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -70,6 +70,7 @@ const Login = () => {
             const user = result.user;
             console.log(user);
             toast.success('Login Successful!');
+            navigate(from, { replace: true });
         })
         .catch(error=>{
             console.error(error);
